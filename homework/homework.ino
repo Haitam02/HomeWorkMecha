b5e191c72943547f6136552492d875b157a53977
Mister-I.Z <20057@ecam.be>
2024-12-05 16:09:57 +0100

    Add using motor

--- homework/homework.ino
+++ homework/homework.ino
@@ -2,7 +2,6 @@
 #include <Adafruit_MotorShield.h>
 #include <NewPing.h>
 
-
 enum RobotState {
   START,
   GO,
@@ -10,13 +9,26 @@ enum RobotState {
   BACK,
 };
 
-
 RobotState currentState = START;
 
-unsigned long startMillis;
-unsigned long currentMillis; 
+// Initialisation du Shield
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Moteur gauche sur MOTOR1
+Adafruit_DCMotor *moteurGauche = AFMS.getMotor(1);
+
+// Moteur droit sur MOTOR2
+Adafruit_DCMotor *moteurDroit = AFMS.getMotor(2);
+
+unsigned long startMillis; 
+unsigned long currentMillis;  
 
 void setup() {
+  // Initialiser le Shield
+  AFMS.begin(); 
+  // Définir la vitesse initiale des moteurs (valeurs entre 0 et 255)
+  moteurGauche->setSpeed(30);
+  moteurDroit->setSpeed(30);
   Serial.begin(9600);
   startMillis = millis();
 }
@@ -28,27 +40,37 @@ void loop() {
     currentState = STOP;
   }
 
+
   switch (currentState) {
     case START:
+      Serial.println("Robot en mode START");
+      moteurGauche->run(RELEASE);
+      moteurDroit->run(RELEASE);
+      }
       
       break;
 
     case GO:
-      
+      Serial.println("Robot en mode GO");
+      moteurGauche->run(FORWARD);
+      moteurDroit->run(FORWARD);
       break;
 
     case BACK:
-      
+      Serial.println("Robot en mode BACK");
+      moteurGauche->run(BACKWARD);
+      moteurDroit->run(BACKWARD);
       break;
 
     case STOP:
-      
+      Serial.println("Robot en mode STOP");
+      moteurGauche->run(RELEASE);
+      moteurDroit->run(RELEASE);
       break;
 
     default:
-      Serial.println("Unknown State");
+      Serial.println("Etat inconnu");
       break;
-  }
-  
+      
+    delay(1000);
 }
-
